fdf963a2bd7f31066e83fa1b23a5b97fc3ce5003
Cory Huebner <chuebner86@gmail.com>
2018-12-06 22:23:11 -0600

    added test coverage

--- src/test/scala/com/bayer/company360/swagger/test/SwaggerConverterSpec.scala
+++ src/test/scala/com/bayer/company360/swagger/test/SwaggerConverterSpec.scala
@@ -14,15 +14,41 @@ class SwaggerConverterSpec extends Spec {
     val allFieldsPopulatedTestCase = getResourceAsFile("all-fields-populated.yaml")
 
     "parse the swagger version" in {
-      val allFieldsParsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
-      allFieldsParsed.swagger should equal("2.0")
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.swagger should equal("2.0")
     }
 
     "parse the info element" in {
-      val allFieldsParsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
-      allFieldsParsed.info.title should equal("A fancy title")
-      allFieldsParsed.info.description should equal(Some("This API will change your life"))
-      allFieldsParsed.info.version should equal("v67")
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.info.title should equal("A fancy title")
+      parsed.info.description should equal(Some("This API will change your life"))
+      parsed.info.version should equal("v67")
+    }
+
+    "parse the host element" in {
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.host should equal("a.domain.com")
+    }
+
+    "parse the schemes element" in {
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.schemes should contain theSameElementsInOrderAs List("https", "http")
+    }
+
+    "parse the base path element" in {
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.basePath should equal("/wacky-cat-pics/v6")
+    }
+
+    "parse the max records element" in {
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      parsed.`x-what-is-maximum-number-of-records-that-could-be-returned` should equal(33)
+    }
+
+    "parse the produces element" in {
+      val parsed = swaggerConverter.parse(allFieldsPopulatedTestCase)
+      val expected = List("application/json", "image/png", "image/gif", "image/jpeg")
+      parsed.produces should contain theSameElementsInOrderAs expected
     }
   }
 }
